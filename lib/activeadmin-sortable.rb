d4180088d4574d1ab8c6c05aec161d575c1d2abb
Adam McCrea <adam@adamlogic.com>
2012-11-09 13:38:48 -0500

    add controller actions and table methods

--- lib/activeadmin-sortable.rb
+++ lib/activeadmin-sortable.rb
@@ -1,7 +1,29 @@
-require "activeadmin-sortable/version"
+require 'activeadmin-sortable/version'
+require 'activeadmin'
 
-module Activeadmin
+module ActiveAdmin
   module Sortable
-    # Your code goes here...
+    module ControllerActions
+      def sortable
+        member_action :sort, :method => :post do
+          resource.insert_at params[:position].to_i
+          head 200
+        end
+      end
+    end
+
+    module TableMethods
+      HANDLE = '&#x2195;'.html_safe
+
+      def sortable_handle_column
+        column '' do |resource|
+          sort_url = url_for([:sort, :admin, resource])
+          content_tag :span, HANDLE, :class => 'handle', 'data-sort-url' => sort_url
+        end
+      end
+    end
   end
+
+  ::ActiveAdmin::ResourceDSL.send(:include, Sortable::ControllerActions)
+  ::ActiveAdmin::Views::TableFor.send(:include, Sortable::TableMethods)
 end
